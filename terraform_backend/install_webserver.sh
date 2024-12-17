c4d48f691dca0a4bfa042afb4440e393ab19341a
Kristijan <kristijan.sarin@gmail.com>
2024-12-17 15:18:22 +0100

    fixed issues, progress

--- terraform_backend/install_webserver.sh
+++ terraform_backend/install_webserver.sh
@@ -8,12 +8,6 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 
 # Create a simple web page
-echo "<html><body><h1>Hello from Terraform and Bash!</h1></body></html>" | sudo tee /var/www/html/index.html
-
-#!/bin/bash
-sudo apt update -y
-sudo apt install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
+echo '<html><body><h1>Hello from Terraform and Bash!</h1></body></html>' | sudo tee /var/www/html/index.html
 
 curl http://localhost
--- terraform_backend/security_groups.tf
+++ terraform_backend/security_groups.tf
@@ -1,5 +1,25 @@
-# Security group
+# ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  name        = "alb_security_group"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for Other Resources (using the ALB Security Group in ingress)
 resource "aws_security_group" "security_group" {
   name        = "security_group_internship_kristijan"
   description = "Security group for ALB"
